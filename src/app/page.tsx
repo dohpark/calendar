37100ac1b923d767763973d85bc4f477026615a8
dohpark <dohpark1129@gmail.com>
2023-10-24 17:05:46 +0900

    feat: #2 Layer 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
+import Layer from '@/components/layouts/Layer';
 import Split from '@/components/layouts/Split';
 
 export default function Home() {
   return (
     <main>
       <Split fraction="auto-start" gap="0">
-        <div className="w-[256px]">1</div>
+        <Layer classExtend={['w-[256px]']} gap="0">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </Layer>
         <div>2</div>
       </Split>
     </main>
---
+++ src/components/layouts/Layer/index.tsx
@@ -0,0 +1,14 @@
+import { gaps } from '@/constants/css';
+import { GapType } from '@/types/css';
+
+interface LayerProps {
+  children: React.ReactNode;
+  gap: GapType;
+  classExtend: string[];
+}
+
+export default function Layer({ children, gap, classExtend }: LayerProps) {
+  const classExtension = classExtend.join(' ');
+
+  return <div className={`grid ${gaps[gap]} ${classExtension}`}>{children}</div>;
+}
