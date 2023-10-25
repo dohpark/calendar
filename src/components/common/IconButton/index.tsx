73f04bf8042a50f3791a85e843130f2a60261983
dohpark <dohpark1129@gmail.com>
2023-10-25 17:58:06 +0900

    feat: #3 IconButton 컴포넌트에 ariaLabel을 prop으로 추가

--- src/components/common/IconButton/index.tsx
+++ src/components/common/IconButton/index.tsx
@@ -2,13 +2,19 @@ interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> 
   children: React.ReactNode;
   classExtend?: string[];
   onClick: () => void;
+  'aria-label': string;
 }
 
-export default function IconButton({ children, classExtend, onClick }: IconButtonProps) {
+export default function IconButton({ children, classExtend, onClick, 'aria-label': ariaLabel }: IconButtonProps) {
   const classExtension = classExtend ? classExtend.join(' ') : '';
 
   return (
-    <button type="button" className={`hover:bg-zinc-100 rounded-full ${classExtension}`} onClick={onClick}>
+    <button
+      type="button"
+      aria-label={ariaLabel}
+      className={`hover:bg-zinc-100 rounded-full ${classExtension}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
