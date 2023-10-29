e687c26824c62f00dc53982fdc86db1630152d7b
dohpark <dohpark1129@gmail.com>
2023-10-29 17:01:28 +0900

    test: #5 날짜 색상 및 텍스트 확인

--- src/components/MonthCalendar/monthcalendar.test.tsx
+++ src/components/MonthCalendar/monthcalendar.test.tsx
@@ -1,6 +1,8 @@
-import { render, screen } from '@/test-utils/testingLibrary';
+import { render, renderHook, screen } from '@/test-utils/testingLibrary';
 import MonthCalendar from '@/components/MonthCalendar';
 import { countMonthDays } from '@/utils/calendar';
+import { useMainCalendar } from '@/store/mainCalendar';
+import { act } from 'react-dom/test-utils';
 
 test('디폴트로 현재 월의 달력을 보인다.', () => {
   render(<MonthCalendar />);
@@ -16,9 +18,45 @@ test('디폴트로 현재 월의 달력을 보인다.', () => {
   expect(calendarButtons).toHaveLength(countDays);
 });
 
-test('지난달과 다음달의 날짜의 색상은 gray400이다. 현재 달의 날짜의 색상은 gray800이다.', () => {});
+test('지난달과 다음달의 날짜의 색상은 gray400이다. 현재 달의 날짜의 색상은 gray800이다.', () => {
+  render(<MonthCalendar />);
 
-test('달의 첫번째 날은 00월 00일로 나타나고 나머지는 00일로 나타난다.', () => {});
+  // 2023년 11월 28일로 날짜 설정
+  const { result } = renderHook(() => useMainCalendar());
+  act(() => result.current.actions.setSelectedDate(new Date(2023, 10, 28)));
+
+  // 현재 달 임의의 날짜 색상 확인
+  const selectedDateButton = screen.getByRole('button', { name: '2023-11-4' });
+  expect(selectedDateButton).toHaveClass('text-gray-800');
+
+  // 화면에 부분적으로 나타난 전 달 임의의 날짜 색상 확인
+  const lastMongthDateButton = screen.getByRole('button', { name: '2023-10-31' });
+  expect(lastMongthDateButton).toHaveClass('text-gray-400');
+
+  // 화면에 부분적으로 나타난 다음 달 임의의 날짜 색상 확인
+  const nextMonthDateButton = screen.getByRole('button', { name: '2023-12-1' });
+  expect(nextMonthDateButton).toHaveClass('text-gray-400');
+});
+
+test('달의 첫번째 날은 00월 00일로 나타나고 나머지는 00일로 나타난다.', () => {
+  render(<MonthCalendar />);
+
+  // 2023년 11월 28일로 날짜 설정
+  const { result } = renderHook(() => useMainCalendar());
+  act(() => result.current.actions.setSelectedDate(new Date(2023, 10, 28)));
+
+  // 2023년 11월 1일 버튼의 텍스트 확인
+  const selectedButton = screen.getByRole('button', { name: '2023-11-28' });
+  expect(selectedButton).toHaveTextContent('28');
+
+  // 2023년 11월 1일 버튼의 텍스트 확인
+  const novemberFirstButton = screen.getByRole('button', { name: '2023-11-1' });
+  expect(novemberFirstButton).toHaveTextContent('11월 1일');
+
+  // 2023년 12월 1일 버튼의 텍스트 확인
+  const decemberFirstButton = screen.getByRole('button', { name: '2023-12-1' });
+  expect(decemberFirstButton).toHaveTextContent('12월 1일');
+});
 
 test('00일 칸을 클릭하면 할일 및 이벤트 생성 모달이 나타난다.', () => {});
 
