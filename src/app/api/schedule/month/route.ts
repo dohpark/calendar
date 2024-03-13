5ec1f4ee339f86a7296c9218fa324b542e984945
dohpark <dohpark1129@gmail.com>
2024-03-14 03:10:24 +0900

    comment: 로직에 대한 주석 추가

--- src/app/api/schedule/month/route.ts
+++ src/app/api/schedule/month/route.ts
@@ -68,21 +68,29 @@ export async function GET(req: Request) {
             ? getDateExcludingTime(new Date(targetSchedule.until.getTime() - 1))
             : getDateExcludingTime(targetSchedule.until);
 
+        // 현재 날짜가 스케줄 시작 날짜와 끝 날짜 사이에 존재
+        // 디폴트 renderType은 'continue', render 값은 0이다.
+        // 오직 start 혹은 startend 만이 render 1이상 7이하의 값을 가질 수 있다.
         if (targetScheduleStartDate <= date && date <= targetScheduleEndDate) {
           let renderType: RenderType = 'continue';
           let render = 0;
 
+          // 스케줄의 시작과 끝 날짜가 현재 날짜와 같은 경우
           if (isSameDate(targetScheduleStartDate, date) && isSameDate(targetScheduleEndDate, date)) {
             renderType = 'startEnd';
             render = 1;
+
+            // 스케줄 시작 날짜와 현재 날짜와 같은 경우 혹은 일요일인 경우 renderType은 'start'다.
           } else if (isSameDate(targetScheduleStartDate, date) || date.getDay() === 0) {
             renderType = 'start';
             render = Math.min(
-              getDateRange(targetScheduleStartDate, targetScheduleEndDate) + 1,
-              getDateRange(date, targetScheduleEndDate) + 1,
-              7 - date.getDay(),
-              7,
+              getDateRange(targetScheduleStartDate, targetScheduleEndDate) + 1, // 2일 ~ 3일의 경우
+              getDateRange(date, targetScheduleEndDate) + 1, // 만약 date가 일요일이고 스케줄 마지막 요일이 월요일인 경우
+              7 - date.getDay(), // 시작 날짜와 끝 날짜간의 차이가 일주일을 넘어가기에 현재날짜와 일주일의 끝의 차
+              7, // 기본적으로 일주일 단위이기에 최대값은 7
             );
+
+            // 스케줄 끝 날짜와 같은 경우 혹은 토요일인 경우
           } else if (isSameDate(targetScheduleEndDate, date) || date.getDay() === 6) {
             renderType = 'end';
             render = 0;
@@ -98,6 +106,7 @@ export async function GET(req: Request) {
       });
     });
 
+    // 스케줄의 상태와 이름에 따라 정렬
     selectedMonthArray.forEach(({ schedules }) =>
       schedules.sort((a, b) => {
         if (a.type !== b.type) {
@@ -108,6 +117,7 @@ export async function GET(req: Request) {
       }),
     );
 
+    // renderOrder 값을 구한다.
     selectedMonthArray.forEach(({ date, renderOrder, schedules }, index) => {
       if (date.getDay() === 0) {
         schedules.forEach(({ id }) => renderOrder.push(id));
@@ -116,25 +126,34 @@ export async function GET(req: Request) {
         const idArr = schedules.map((schedule) => schedule.id);
         const deque: number[] = [];
         const prevRenderOrder = selectedMonthArray[index - 1].renderOrder;
-        prevRenderOrder.forEach((id, index2) => {
+
+        // 전 날짜의 renderOrder을 돌며 현재 날짜 스케줄에도 포함하는 스케줄 id를 newOrder에 push 한다.
+        // 만약 전 날 존재했지만 현재 날짜 스케줄 상 없으면 -1를 push한다.
+        // deque에 newOrder 내에 -1값의 index를 저장한다.
+        prevRenderOrder.forEach((id, prevRenderOrderIndex) => {
           if (idArr.includes(id)) newOrder.push(id);
           else {
             newOrder.push(-1);
-            deque.push(index2);
+            const nullIndex = prevRenderOrderIndex;
+            deque.push(nullIndex);
           }
         });
 
+        // 전 날에 없고 현재 날짜에 생긴 새로운 스케줄들을 newOrder에 저장한다.
+        // newOrder에 값이 -1인 칸부터 채워 넣는다. (deque를 활용하여 확인 가능)
+        // 만약에 newOrder에 남는 -1 칸이 없다면 push로 추가한다.
         idArr.forEach((id) => {
           if (!prevRenderOrder.includes(id)) {
             if (deque.length) {
-              const index3 = deque.shift()!;
-              newOrder[index3] = id;
+              const nullIndex = deque.shift()!;
+              newOrder[nullIndex] = id;
             } else {
               newOrder.push(id);
             }
           }
         });
 
+        // newOrder 배열의 가장 오른쪽에 -1값을 지니면 무의미하기에 유의미한 id값을 마지막 index에 지닐 수 있도록 pop한다.
         while (newOrder.length) {
           const id = newOrder.pop()!;
           if (id !== -1) {
